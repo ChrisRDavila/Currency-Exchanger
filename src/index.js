dae4e7f14253396ea3b3f1c235e595bb74cb95b3
ChrisRDavila <crossdavila@gmail.com>
2023-04-21 09:41:38 -0700

    return Promise for CurrencyExchange.getRate in index.js and error handling set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-//business
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import CurrencyExchange from './exchanger.js';
+
+// Business Logic
+
+function getRate(currency) {
+  let promise = CurrencyExchange.getRate(currency);
+  promise.then(function(rateInfo) {
+    displayExchange(rateInfo);
+  }, function(rateError) {
+    printError(rateError);  
+  });
+
+}
