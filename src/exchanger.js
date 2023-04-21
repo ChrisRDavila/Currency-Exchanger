4dc6daaf2ca54d55a6cb797d910a45625c81d322
ChrisRDavila <crossdavila@gmail.com>
2023-04-21 10:54:10 -0700

    update paramters to match how i want API to react to submission

--- src/exchanger.js
+++ src/exchanger.js
@@ -1,14 +1,14 @@
 export default class CurrencyExchange {
-  static getRate(currency) {
+  static getRate(currency, type) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       request.addEventListener("load",function() {
-        const currencyType = JSON.parse(this.responseText);
+        const resultCurrency = JSON.parse(this.responseText);
         if (this.status === 200) {
-          resolve([currencyType, currency]);
+          resolve([resultCurrency, currency, type]);
         } else {
-          reject([this, currencyType, currency]);
+          reject([this, resultCurrency, currency, type]);
         }
       });
       request.open("GET", url, true);
--- src/index.js
+++ src/index.js
@@ -15,3 +15,16 @@ function getRate(currency) {
 }
 
 // UI Logic
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  let currencyAmount = document.querySelector('#currency-amount').value;
+  let currencyType = document.querySelector9('#currency-type').value 
+  const errorMessage = document.getElementById("error-message");
+  getRate(currencyAmount, currencyType);
+}
+
+window.addEventListener("load", function() {
+  this.document.querySelector("form").addEventListener("submit", handleFprmSubmission);
+})
+
