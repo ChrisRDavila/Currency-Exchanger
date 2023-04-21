9bd5d701ab0293e0e7967ef18daa2beb871c6c7e
ChrisRDavila <crossdavila@gmail.com>
2023-04-21 12:52:43 -0700

    took out loop, attempting to build and run to use dev tools to fnd errors

--- src/index.js
+++ src/index.js
@@ -17,32 +17,39 @@ function getRate(currency, type) {
 // UI Logic
 
 function displayExchange(data) {
+  let currencyAmount = document.querySelector('#currency-amount').value;
+  let currencyType = document.querySelector('#currency-type').value;
   const rates = data[0];
-  let results = "";
-
-  for(let i=0; i=data[0].length; i++) {
-    const rate = rates[i];
-    const resultRate = rate.conversion_rates[currencyType]
-    const resultAmount  = resultRate * currencyAmount
-    
-    results = `<p>Your exchange:<p>
-    <p>in ${currencyType} equals ${resultAmount}<p>`
-  }
-  const showResults = document.querySelector('show-results');
-  showResults.innerHTML = `Here are your results:
-  ${results}`;
-
+  const resultRate = rates.conversion_rates[currencyType];
+  const resultAmount  = resultRate * currencyAmount;
+  document.querySelector("#show-result").innerHTML = `<p>Your exchange:<p>
+  <p>in ${currencyType} equals ${resultAmount}<p>`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
   let currencyAmount = document.querySelector('#currency-amount').value;
-  let currencyType = document.querySelector9('#currency-type').value 
+  let currencyType = document.querySelector('#currency-type').value; 
   // const errorMessage = document.getElementById("error-message");
   getRate(currencyAmount, currencyType);
 }
 
+function printError(error) {
+  document.querySelector('#show-results').innerText = `There was an error accessing our exchange for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+}
+
 window.addEventListener("load", function() {
   this.document.querySelector("form").addEventListener("submit", handleFormSubmission);
-})
+});
+
+
+// for(let i=0; i=data[0].length; i++) {
+  
+  
+//   results += `<p>Your exchange:<p>
+//   <p>in ${currencyType} equals ${resultAmount}<p>`
+// }
+// const showResults = document.querySelector('show-results');
+// showResults.innerHTML = `Here are your results:
+// ${results}`;
 
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Currency Exchange',
       template: './src/index.html',
       inject: 'body'
     })
