569aa5ae4e834219db738553c63d0c7da8f4b77e
ChrisRDavila <crossdavila@gmail.com>
2023-04-21 19:39:53 -0700

    made a mess

--- src/exchanger.js
+++ src/exchanger.js
@@ -1,18 +1,31 @@
 export default class CurrencyExchange {
-  static getRate(amount, type) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-      request.addEventListener("loadend",function() {
-        const resultCurrency = JSON.parse(this.responseText);
-        if (this.status === 200) {
-          resolve([resultCurrency, amount, type]);  
+  static getRate() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
         } else {
-          reject([this, resultCurrency, type]);
-        }
+          return response.json();
+        }  
+      })
+      .catch(function(error){
+        return error;
       });
-      request.open("GET", url, true);
-      request.send();
-    });
   }
 }
+
+// new Promise(function(resolve, reject) {
+//   let request = new XMLHttpRequest();
+//   const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+//   request.addEventListener("loadend",function() {
+//     const resultCurrency = JSON.parse(this.responseText);
+//     if (this.status === 200) {
+//       resolve([resultCurrency, amount, type]);  
+//     } else {
+//       reject([this, resultCurrency, type]);
+//     }
+//   });
+//   request.open("GET", url, true);
+//   request.send();
+// });
--- src/index.js
+++ src/index.js
@@ -6,27 +6,29 @@ import CurrencyExchange from './exchanger.js';
 // Business Logic
 
 function getRate(amount, type) {
-  let promise = CurrencyExchange.getRate(amount, type);
-  promise.then(function(data) {
-    displayExchange(data);
-  }, function(error) {
-    printError(error);  
-  });
+  CurrencyExchange.getRate(amount, type)
+    .then(function(response, amount, type) {
+      if (response.conversion_rates) {
+        displayExchange(response, amount, type);
+      } else {
+        printError(response, amount, type);
+      }
+    });
 }
 
 // UI Logic
 
-function displayExchange(data) {
-  let currencyType = document.querySelector('#currency-type').value;
-  let currencyAmount = document.querySelector('#currency-amount').value;
-  const rates = data[0];
-  const resultRate = rates.conversion_rates[currencyType];
-  const resultAmount  = resultRate * currencyAmount;
+function displayExchange(response, type, amount) {
+  const currencyType = document.querySelector('#currency-type').value;
+  const currencyAmount = document.querySelector('#currency-amount').value;
+  const rates = response.conversion_rates;
+  let resultRate = rates[currencyType];
+  let resultAmount  = resultRate * currencyAmount;
   if (rates.conversion_rates[currencyType] === undefined) {
-    document.querySelector('#show-results').innerText = `${data[2]} is not a real country code`
+    document.querySelector('#show-results').innerText = `${type} is not a real country code`;
   } else { 
-    document.querySelector("#show-results").innerHTML = `<p>Your exhange from ${data[1]}$ USD<p>
-    <p>in ${data[2]} equals ${resultAmount}<p>`;
+    document.querySelector("#show-results").innerHTML = `<p>Your exchange from ${currencyAmount}$ USD<p>
+    <p>in ${type} equals ${resultAmount}<p>`;
   }  
 }
 
@@ -34,16 +36,25 @@ function handleFormSubmission(event) {
   event.preventDefault();
   const type = document.querySelector('#currency-type').value; 
   const amount = document.querySelector('#currency-amount').value;
+  document.querySelector('#currency-type').value = null;
+  document.querySelector('#currency-amount').value = null;
   getRate(amount, type);
 }
 
-function printError(error) {
-  document.querySelector('#show-results').innerText = `There was an error accessing our exchange for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+function printError(error, type) {
+  document.querySelector('#show-results').innerText = `There was an error accessing our exchange for ${type}:
+${error}`;
 }
 
 window.addEventListener("load", function() {
   this.document.querySelector("form").addEventListener("submit", handleFormSubmission);
 });
 //WIP, round decimals to 100th and reset values to null after input so can reuse withput browser refresh"
-//WIP finish readme and erase unused files
 
+
+// let promise = CurrencyExchange.getRate(amount, type);
+//   promise.then(function(data) {
+//     displayExchange(data);
+//   }, function(error) {
+//     printError(error);  
+//   });
